c075c7a819f4117a894c50ab4f1b636fdaa9eab2
cedricjoseph <work.cedricjoseph@gmail.com>
2020-04-16 00:13:41 +0200

    Pages prêtes

--- foodfacts/urls.py
+++ foodfacts/urls.py
@@ -2,5 +2,10 @@ from . import views
 from django.urls import path
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', views.home, name='home'),
+    path('aliment', views.aliment, name='aliment'),
+    path('resultats', views.resultats, name='resultats'),
+    path('favourites', views.favourites, name='favourites'),
+    path('account', views.account, name='account'),
+    path('notice', views.notice, name='notice'),
 ]
--- foodfacts/views.py
+++ foodfacts/views.py
@@ -3,5 +3,20 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 
-def index(request):
-    return render(request, "index.html")
+def home(request):
+    return render(request, "accueil.html")
+
+def aliment(request):
+    return render(request, "aliment.html")
+
+def resultats(request):
+    return render(request, "resultats.html")
+
+def favourites(request):
+    return render(request, "mes_aliments.html")
+
+def account(request):
+    return render(request, "mon_compte.html")
+
+def notice(request):
+    return render(request, "mentions_legales.html")
