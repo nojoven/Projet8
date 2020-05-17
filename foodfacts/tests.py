5183e8ed97c74246ef3207552d5e6734a5acc76d
cedricjoseph <work.cedricjoseph@gmail.com>
2020-05-17 13:21:33 +0200

    I moved PurBeurre tests

---
+++ foodfacts/tests.py
@@ -0,0 +1,96 @@
+from django.test import TestCase
+
+from foodfacts.models import Products
+from foodfacts.modules.database_service import DatabaseService
+
+
+class SimpleTest(TestCase):
+    URI_f_BASE = "http://localhost:8000/foodfacts/"
+    home_request = URI_f_BASE
+    aliment_request = f"{URI_f_BASE}aliment/1/"
+    notice_request = f"{URI_f_BASE}notice/"
+    resultats_gazpacho = f"{URI_f_BASE}resultats/Gazpacho/"
+    resultats_empty = f"{URI_f_BASE}resultats/empty/"
+    favourites_request = f"{URI_f_BASE}favourites/"
+    account_request = f"{URI_f_BASE}account/"
+
+    def test_views_home(self):
+        response = self.client.get(self.home_request)
+        self.assertEqual(response.status_code, 200)
+
+    def test_views_aliment(self):
+        response = self.client.get(self.aliment_request)
+        self.assertEqual(response.status_code, 200)
+
+    def test_views_resultats_gazpacho(self):
+        response = self.client.get(self.resultats_gazpacho)
+        self.assertEqual(response.status_code, 200)
+
+    def test_views_resultats_empty(self):
+        response = self.client.get(self.resultats_empty)
+        self.assertEqual(response.status_code, 200)
+
+    def test_views_notice(self):
+        response = self.client.get(self.notice_request)
+        self.assertEqual(response.status_code, 200)
+
+    def test_select_better_products(self):
+        selection = DatabaseService.select_better_products("Gazpacho", "soup", -4)
+        assert selection is not None
+
+    def test_search_product(self):
+        product = {
+            "category": "soup",
+            "stores": "Auchan",
+            "brands": "Reflets",
+            "productname": "Gazpacho",
+            "nutrigrade": "a",
+            "quantity": "1L",
+            "front_img": "img.jpg",
+            "nutrition_img": "img.png",
+            "ingredients_img": "img.svg",
+            "fat_100g": 0.20,
+            "sugars_100g": 3.20,
+            "saturated_fat_100g": 0.20,
+            "energy_kcal_100g": 250.16,
+            "nutrition_Score_100g": -4,
+            "fiber_100g": 24.14,
+            "salt_100g": 1.03,
+            "proteins_100g": 5.63,
+            "carbs_100g": 3.20,
+            "sodium_100g": 1.03,
+            "url": "www.url.com"
+        }
+        query = Products(**product)
+        query.save()
+        searched = DatabaseService.select_product("Gazpacho")
+        assert searched is not None
+
+    def test_show_details(self):
+        product = {
+            "category": "soup",
+            "stores": "Auchan",
+            "brands": "Reflets",
+            "productname": "Gazpacho",
+            "nutrigrade": "a",
+            "quantity": "1L",
+            "front_img": "img.jpg",
+            "nutrition_img": "img.png",
+            "ingredients_img": "img.svg",
+            "fat_100g": 0.20,
+            "sugars_100g": 3.20,
+            "saturated_fat_100g": 0.20,
+            "energy_kcal_100g": 250.16,
+            "nutrition_Score_100g": -4,
+            "fiber_100g": 24.14,
+            "salt_100g": 1.03,
+            "proteins_100g": 5.63,
+            "carbs_100g": 3.20,
+            "sodium_100g": 1.03,
+            "url": "www.url.com"
+        }
+        query = Products(**product)
+        query.save()
+        article = DatabaseService.select_product("Gazpacho")
+        selected = DatabaseService.show_details(article.idproduct)
+        assert selected is not None
--- roles/tests.py
+++ roles/tests.py
@@ -1,64 +1,42 @@
-"""This file is used to test the functions of the two apps. It is called via the pytest command in the terminal"""
-from django.contrib.auth import authenticate
 import pytest
-import requests as re
+from django.contrib.auth import authenticate
+from django.test import TestCase
 from foodfacts.models import Favorites, Products
 from foodfacts.modules.database_service import DatabaseService
 
 
-@pytest.mark.django_db
-class TestPurBeurre:
-    """Pytest will be used to verify the behaviour of the following functions"""
-    URI_f_BASE = "http://localhost:8000/foodfacts/"
+class SimpleTest(TestCase):
+
     URI_r_BASE = "http://localhost:8000/roles/"
     provided_mail = "dubosc@gmail.com"
     provided_password = "Franck"
-
-    home_request = URI_f_BASE
-    aliment_request = f"{URI_f_BASE}aliment/1/"
-    favourites_request = f"{URI_f_BASE}favourites/"
-    account_request = f"{URI_r_BASE}account/"
     signin_request = f"{URI_r_BASE}signin/"
-    notice_request = f"{URI_f_BASE}notice/"
-    resultats_gazpacho = f"{URI_f_BASE}resultats/Gazpacho/"
-    resultats_empty = f"{URI_f_BASE}resultats/empty/"
+    favourites_request = f"{URI_r_BASE}favourites/"
+    account_request = f"{URI_r_BASE}account/"
     register_request = f"{URI_r_BASE}register/"
 
-    def test_views_home(self):
-        response = re.get(self.home_request)
-        assert response.status_code == 200
-
-    def test_views_aliment(self):
-        response = re.get(self.aliment_request)
-        assert response.status_code == 200
-
-    def test_views_resultats_gazpacho(self):
-        response = re.get(self.resultats_gazpacho)
-        assert response.status_code == 200
-
-    def test_views_resultats_empty(self):
-        response = re.get(self.resultats_empty)
+    def test_views_signin(self):
+        response = self.client.get(self.signin_request)
         assert response.status_code == 200
 
     def test_views_favourites(self):
-        response = re.get(self.resultats_gazpacho)
+        response = self.client.get(self.favourites_request)
         assert response.status_code == 200
 
     def test_views_account(self):
-        response = re.get(self.account_request)
-        assert response.status_code == 200
-
-    def test_views_signin(self):
-        response = re.get(self.signin_request)
+        response = self.client.get(self.account_request)
         assert response.status_code == 200
 
     def test_views_register(self):
-        response = re.get(self.register_request)
+        response = self.client.get(self.register_request)
         assert response.status_code == 200
 
-    def test_views_notice(self):
-        response = re.get(self.notice_request)
-        assert response.status_code == 200
+
+@pytest.mark.django_db
+class TestRoles:
+    """Pytest will be used to verify the behaviour of the following functions"""
+    provided_mail = "dubosc@gmail.com"
+    provided_password = "Franck"
 
     def test_create_product(self):
         product = {
@@ -138,67 +116,6 @@ class TestPurBeurre:
 
         except KeyError:
             return KeyError("CANNOT UPDATE A USER WHO DOES NOT EXISTS")
-
-    def test_select_better_products(self):
-        selection = DatabaseService.select_better_products("Gazpacho", "soup", -4)
-        assert selection is not None
-
-    def test_search_product(self):
-        product = {
-            "category": "soup",
-            "stores": "Auchan",
-            "brands": "Reflets",
-            "productname": "Gazpacho",
-            "nutrigrade": "a",
-            "quantity": "1L",
-            "front_img": "img.jpg",
-            "nutrition_img": "img.png",
-            "ingredients_img": "img.svg",
-            "fat_100g": 0.20,
-            "sugars_100g": 3.20,
-            "saturated_fat_100g": 0.20,
-            "energy_kcal_100g": 250.16,
-            "nutrition_Score_100g": -4,
-            "fiber_100g": 24.14,
-            "salt_100g": 1.03,
-            "proteins_100g": 5.63,
-            "carbs_100g": 3.20,
-            "sodium_100g": 1.03,
-            "url": "www.url.com"
-        }
-        query = Products(**product)
-        query.save()
-        searched = DatabaseService.select_product("Gazpacho")
-        assert searched is not None
-
-    def test_show_details(self):
-        product = {
-            "category": "soup",
-            "stores": "Auchan",
-            "brands": "Reflets",
-            "productname": "Gazpacho",
-            "nutrigrade": "a",
-            "quantity": "1L",
-            "front_img": "img.jpg",
-            "nutrition_img": "img.png",
-            "ingredients_img": "img.svg",
-            "fat_100g": 0.20,
-            "sugars_100g": 3.20,
-            "saturated_fat_100g": 0.20,
-            "energy_kcal_100g": 250.16,
-            "nutrition_Score_100g": -4,
-            "fiber_100g": 24.14,
-            "salt_100g": 1.03,
-            "proteins_100g": 5.63,
-            "carbs_100g": 3.20,
-            "sodium_100g": 1.03,
-            "url": "www.url.com"
-        }
-        query = Products(**product)
-        query.save()
-        article = DatabaseService.select_product("Gazpacho")
-        selected = DatabaseService.show_details(article.idproduct)
-        assert selected is not None
 
     def test_find_user_category_favourites(self):
         favourites = DatabaseService.sort_favourites(4, "soup")
