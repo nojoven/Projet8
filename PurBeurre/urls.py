031ec41b368229d4b0e0ab73776974f66c46620e
cedricjoseph <work.cedricjoseph@gmail.com>
2020-05-23 16:10:35 +0200

    correct base url base

--- PurBeurre/urls.py
+++ PurBeurre/urls.py
@@ -19,6 +19,8 @@ from django.urls import include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("/", include("foodfacts.urls")),
+    path("", include("foodfacts.urls")),
     path("foodfacts/", include("foodfacts.urls")),
     path("roles/", include("roles.urls")),
 ]
