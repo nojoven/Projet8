2a44b3c75d9c842159efcf802814d80acd731d83
cedricjoseph <work.cedricjoseph@gmail.com>
2020-05-24 10:52:21 +0200

    Ajout du document sur les tests dans le dossier du projet

--- foodfacts/test_foodfacts.py
+++ foodfacts/test_foodfacts.py
@@ -1,5 +1,5 @@
 from django.test import TestCase
-
+from PurBeurre.constants import PRODUCT_EXAMPLE
 from foodfacts.models import Products
 from foodfacts.modules.database_service import DatabaseService
 
@@ -39,28 +39,7 @@ class SimpleTest(TestCase):
         assert selection is not None
 
     def test_search_product(self):
-        product = {
-            "category": "soup",
-            "stores": "Auchan",
-            "brands": "Reflets",
-            "productname": "Gazpacho",
-            "nutrigrade": "a",
-            "quantity": "1L",
-            "front_img": "img.jpg",
-            "nutrition_img": "img.png",
-            "ingredients_img": "img.svg",
-            "fat_100g": 0.20,
-            "sugars_100g": 3.20,
-            "saturated_fat_100g": 0.20,
-            "energy_kcal_100g": 250.16,
-            "nutrition_Score_100g": -4,
-            "fiber_100g": 24.14,
-            "salt_100g": 1.03,
-            "proteins_100g": 5.63,
-            "carbs_100g": 3.20,
-            "sodium_100g": 1.03,
-            "url": "www.url.com",
-        }
+        product = PRODUCT_EXAMPLE
         query = Products(**product)
         query.save()
         searched = DatabaseService.select_product("Gazpacho")
