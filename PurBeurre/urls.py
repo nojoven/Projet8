624c66f9c0edf7d262ccb6dd1fd2b2e4c27dd0d8
Joseph <work.cedricjoseph@gmail.com>
2020-11-22 18:23:53 +0100

    apparently modified

--- PurBeurre/urls.py
+++ PurBeurre/urls.py
@@ -17,10 +17,15 @@ from django.contrib import admin
 from django.urls import path
 from django.urls import include
 
+
+def trigger_error(request):
+    division_by_zero = 1 / 0
+
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("/", include("foodfacts.urls")),
     path("", include("foodfacts.urls")),
     path("foodfacts/", include("foodfacts.urls")),
     path("roles/", include("roles.urls")),
+    path('sentry-debug/', trigger_error)
 ]
